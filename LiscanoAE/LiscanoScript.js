9dac57f775b0e518f718784ae92a466443f4dbcc
Saiyaneren <yhouwneespinosa@gmail.com>
2022-10-10 17:47:37 +0800

    edited the contents in LiscanoScript.js

---
+++ LiscanoAE/LiscanoScript.js
@@ -0,0 +1,49 @@
+//DEV-PROJECTS VIDEO PLAYER
+var videoPlayer = document.getElementById("videoPlayer");
+var myVideo = document.getElementById("myVideo");
+
+function stopVideo() {
+    videoPlayer.style.display = "none";
+}
+
+function playVideo(file) {
+    myVideo.src = file;
+    videoPlayer.style.display = "block";
+}
+
+//BUSINESS-BLOG
+var businessVideoPlayer = document.getElementById("businessVideoPlayer");
+var myBusinessVideo = document.getElementById("myBusinessVideo");
+
+function busStopVideo() {
+    businessVideoPlayer.style.display = "none";
+}
+
+function busPlayVideo(file) {
+    myBusinessVideo.src = file;
+    businessVideoPlayer.style.display = "block";
+}//DEV-PROJECTS VIDEO PLAYER
+    var videoPlayer = document.getElementById("videoPlayer");
+    var myVideo = document.getElementById("myVideo");
+
+    function stopVideo() {
+        videoPlayer.style.display = "none";
+    }
+
+    function playVideo(file) {
+        myVideo.src = file;
+        videoPlayer.style.display = "block";
+    }
+
+    //BUSINESS-BLOG
+    var businessVideoPlayer = document.getElementById("businessVideoPlayer");
+    var myBusinessVideo = document.getElementById("myBusinessVideo");
+
+    function busStopVideo() {
+        businessVideoPlayer.style.display = "none";
+    }
+
+    function busPlayVideo(file) {
+        myBusinessVideo.src = file;
+        businessVideoPlayer.style.display = "block";
+    }
