ab7a18a48e462e6067dbac4b3787fa5a92a704a9
jaetmbl <107400739+jaetmbl@users.noreply.github.com>
2022-10-16 22:03:10 +0800

    Revert "header inserted"

--- AlcalaGN/AlcalaJS.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    //for menu navbar
-    $('.mbutton').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.mbutton i').toggleClass("active");
-    })
-});
